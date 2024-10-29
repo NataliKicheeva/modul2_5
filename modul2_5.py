804e8334bd252d1eec96e5b7d64b7f7d6be80378
NataliKicheeva <tusya21po@gmail.com>
2024-10-29 20:08:34 +0700

    Update modul2_5.py

--- modul2_5.py
+++ modul2_5.py
@@ -1,9 +1,3 @@
-#result1 = get_matrix(2, 2, 10)
-#result2 = get_matrix(3, 5, 42)
-#result3 = get_matrix(4, 2, 13)
-#print(result1)
-#print(result2)
-#print(result3)
 def get_matrix(n, m, value):
     matrix = []
     if n <= 0 or m <= 0:
